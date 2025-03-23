d3d9dd3f4adfe8aea409e57ce4759de2e1c047ba
30lima <pedrolimaa354@gmail.com>
2025-03-23 19:15:34 -0300

    style: add style

---
+++ fokus/app/index.jsx
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      styles={styles.container}
+    >
+      <Text style={style.text}>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: '#021123',
+  },
+
+  test: {
+    color: '#FFF',
+  }
+
+})
